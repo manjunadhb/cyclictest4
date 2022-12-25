170a091d4fd4d5a5701e267e2b6a6cf1db5df08c
manjunadhb <manjunadhb@gmail.com>
2022-12-25 07:27:01 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [hitMovies, setHitMovies] = useState([]);
+
+  let getHitMoviesList = async () => {
+    let response = await axios.get("http://localhost:3434/hitMovies");
+
+    setHitMovies(response.data);
+  };
+
+  return (
+    <div className="App">
+      <h1>Version 4</h1>
+      <button
+        onClick={() => {
+          getHitMoviesList();
+        }}
+      >
+        Get Hit Movies
+      </button>
+      {hitMovies.map((name) => {
+        return <h1>{name}</h1>;
+      })}
+    </div>
+  );
+}
+
+export default App;
